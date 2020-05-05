5c2b2ce5be1857cbcd2aa8c342c12d9505f62447
Alexander Schlögl <alexander.schloegl@uibk.ac.at>
2020-05-05 21:34:37 +0200

    Added install and uninstall scripts

---
+++ install-ubuntu.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# install kernel headers
+sudo apt-get install linux-headers-$(uname -r)
+
+make
+
+sudo mkdir -p "/lib/modules/"`uname -r`"/kernel/drivers/intel/sgx"    
+sudo cp isgx.ko "/lib/modules/"`uname -r`"/kernel/drivers/intel/sgx"    
+sudo sh -c "cat /etc/modules | grep -Fxq isgx || echo isgx >> /etc/modules"    
+sudo /sbin/depmod
+sudo /sbin/modprobe isgx
